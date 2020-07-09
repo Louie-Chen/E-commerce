7cda9b79c4bbe4b1493d79e64fe5348bea27877a
textwork <max0100618@gmail.com>
2020-07-09 12:48:37 +0800

    new

--- stylesheets/Js/index-all.js
+++ stylesheets/Js/index-all.js
@@ -5,29 +5,7 @@ $(document).ready(function () {
 });
 
 
-var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay:{
-    delay: 1500,
-    },
-    effect:"cube",
-    speed:3000,
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    
-  })
 
 /*首頁*/
 var content = document.querySelector('.content');
